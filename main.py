d8bd63f098ce18360dd8ad412b6b6d5bb021f955
XFireV <thaleshunemeyer@gmail.com>
2024-06-29 17:35:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,213 +1,215 @@
 import random
+import tkinter as tk
 
-while True:
-    TE = 10
+PARTES_FORCA = [
+    '''
+
+    ''',
+    '''
+     |     
+     |    
+     |    
+     |    
+    _|_
+    ''',
+    '''
+     _
+     |     
+     |    
+     |    
+     |    
+    _|_
+    ''',
+    '''
+     _______
+     |     
+     |    
+     |    
+     |    
+    _|_
+    ''',
+    '''
+     _______
+     |     |
+     |    
+     |    
+     |    
+    _|_
+    ''',
+    '''
+     _______
+     |     |
+     |     O
+     |    
+     |    
+    _|_
+    ''',
+    ''' 
+     _______
+     |     |
+     |     O
+     |     |
+     |    
+    _|_
+    ''',
+    '''
+     _______
+     |     |
+     |     O
+     |    /|
+     |    
+    _|_
+    ''',
+    '''
+     _______
+     |     |
+     |     O
+     |    /|\\
+     |    
+    _|_
+    ''',
+    '''
+     _______
+     |     |
+     |     O
+     |    /|\\
+     |    / 
+    _|_
+    ''',
+    '''
+     _______
+     |     |
+     |     O
+     |    /|\\
+     |    / \\
+    _|_
+    '''
+]
+
+def atualizar_forca():
+    if T <= 10:
+        canvas.delete("all")
+        canvas.create_text(400, 150, text=PARTES_FORCA[T], anchor="center", font=("Courier", 18))
+
+
+def verificar_letra(letra):
+    global T, LU, PE, F, FR
+
+    L = letra.lower() 
+
+    if L in LU:
+        aviso_letra_usada.config(text='Letra já utilizada!')
+        return
+
+    LU.add(L)
+    aviso_letra_usada.config(text='')
+
+    acertou = False
+    for i in range(FR):
+        if F[i].lower() == L:
+            PE[i] = F[i]
+            acertou = True
+
+    if not acertou:
+        T += 1
+        atualizar_forca() 
+        if T >= 10:
+            resultado_jogo.config(text=f'Você perdeu! A palavra era {F}.')
+            btn_novo_jogo.place(relx=0.5, rely=0.95, anchor="center") 
+            for btn in botoes_letras:
+                btn.config(state="disabled")
+            return
+
+    if F == ''.join(PE):
+        resultado_jogo.config(text=f'Parabéns, você adivinhou!!! A palavra era {F}.')
+        btn_novo_jogo.place(relx=0.5, rely=0.95, anchor="center")
+        for btn in botoes_letras:
+            btn.config(state="disabled")
+        return
+
+    label_palavra.config(text=' '.join(PE))
+
+def iniciar_novo_jogo():
+    global T, F, FR, PE, LU
+
     T = 0
-    Animais = [
-        'gato', 'cachorro', 'elefante', 'girafa', 'leão', 'macaco', 'panda',
-        'tigre', 'urso', 'zebra', 'cobra', 'rato', 'galinha', 'porco'
-    ]
-    Frutas = [
-        'maca', 'banana', 'abacaxi', 'laranja', 'cereja', 'melao', 'abacate',
-        'morango', 'kiwi', 'jabuticaba', 'amendoim', 'amendôa', 'amora',
-        'ameixa', 'pitaia'
-    ]
-    Biomas = [
-        'savana', 'deserto', 'montanha', 'floresta', 'praia', 'pantanal',
-        'mar', 'oceano', 'pradaria', 'planície', 'planalto', 'caverna',
-        'tundra', 'taiga', 'temperado', 'tropical'
-    ]
-    TUDO = [Animais, Frutas, Biomas]
     E = random.choice(TUDO)
     if E == Animais:
         F = random.choice(Animais)
-        print(f'O tema será Animais')
+        dica = "Tema: Animais"
     elif E == Frutas:
         F = random.choice(Frutas)
-        print(f'O tema será Frutas')
+        dica = "Tema: Frutas"
     elif E == Biomas:
         F = random.choice(Biomas)
-        print(f'O tema será Biomas')
-    print()
+        dica = "Tema: Biomas"
+
     FR = len(F)
     PE = ['_'] * FR
     LU = set()
-    while T < 10:
-        print(' '.join(PE))
-        L = input('Digite 1 letra: ').lower()
-        while len(L) != 1 or not L.isalpha() or L in LU:
-            L = input(
-                'Por favor, digite apenas 1 letra que ainda não foi usada: ')
-        LU.add(L)
-        print(LU)
-        if L == '!':
-            print(f'A dica é {F}')
-            continue
-        acertou = False
-        for i in range(FR):
-            if F[i].lower() == L:
-                PE[i] = F[i]
-                acertou = True
-        if not acertou:
-            T += 1
-            if T == 1:
-                TE -= 1
-                print('''
-|_______
-|    0
-|   
-|  
-|    
-|   
-|   
-|_________
-''')
-                print('\n'
-                      f'você ainda tem {TE} tentativas'
-                      '\n')
-            elif T == 2:
-                print('''
-    |_______
-    |    0
-    |    |
-    |  
-    |   
-    |   
-    |   
-    |_________
-    ''')
-                TE -= 1
-                print('\n'
-                      f'você ainda tem {TE} tentativas'
-                      '\n')
-            elif T == 3:
-                print('''
-    |_______
-    |    0
-    |    |
-    |    |
-    |    
-    |   
-    |   
-    |_________
-    ''')
-                TE -= 1
-                print('\n'
-                      f'você ainda tem {TE} tentativas'
-                      '\n')
-            elif T == 4:
-                print('''
-    |_______
-    |    0
-    |    |
-    |    | 
-    |    |
-    |   
-    |  
-    |_________
-    ''')
-                TE -= 1
-                print('\n'
-                      f'você ainda tem {TE} tentativas'
-                      '\n')
-            elif T == 5:
-                print('''
-    |_______
-    |    0
-    |   ||
-    |  | |
-    |    |
-    |   
-    |  
-    |_________
-    ''')
-                TE -= 1
-                print('\n'
-                      f'você ainda tem {TE} tentativas'
-                      '\n')
-            elif T == 6:
-                print('''
-    |_______
-    |    0
-    |   ||| 
-    |  | | |
-    |    |
-    |   
-    |   
-    |_________
-    ''')
-                TE -= 1
-                print('\n'
-                      f'você ainda tem {TE} tentativas'
-                      '\n')
-            elif T == 7:
-                print('''
-    |_______
-    |    0
-    |   ||| 
-    |  | | |
-    |    |
-    |   | 
-    |   
-    |_________
-    ''')
-                TE -= 1
-                print('\n'
-                      f'você ainda tem {TE} tentativas'
-                      '\n')
-            elif T == 8:
-                print('''
-    |_______
-    |    0
-    |   ||| 
-    |  | | |
-    |    |
-    |   | 
-    |   | 
-    |_________
-    ''')
-                TE -= 1
-                print('\n'
-                      f'você ainda tem {TE} tentativas'
-                      '\n')
-            elif T == 9:
-                print('''
-    |_______
-    |    0
-    |   ||| 
-    |  | | |
-    |    |
-    |   | |
-    |   | 
-    |_________
-    ''')
-                TE -= 1
-                print('\n'
-                      f'você ainda tem {TE} tentativa restante'
-                      '\n')
-            elif T == 10:
-                print('''
-    |_______
-    |    0
-    |   ||| 
-    |  | | |
-    |    |
-    |   | |
-    |   | |
-    |_________
-    ''')
-            if T >= 10:
-                print(f'Você perdeu! A palavra era {F}')
-                break
-        if F == ''.join(PE):
-            print(f'Parabéns, você adivinhou!!! A palavra era {F}')
-            break
-    print()
-    print('Deseja jogar novamente?')
-    print('1 - Sim')
-    print('2 - Não')
-    J = input('Digite sua escolha: ')
-    print()
-    if J != '1':
-        break
 
+    label_palavra.config(text=' '.join(PE))
+    label_dica.config(text=dica)
+    resultado_jogo.config(text='')
+    aviso_letra_usada.config(text='')
+    atualizar_forca()
+
+    btn_novo_jogo.place_forget() 
+    for btn in botoes_letras:
+        btn.config(state="normal")
+
+
+janela = tk.Tk()
+janela.title("Jogo de Adivinhação")
+
+largura_tela = janela.winfo_screenwidth()
+altura_tela = janela.winfo_screenheight()
+janela.geometry(f"{largura_tela}x{altura_tela}+0+0")
+
+canvas = tk.Canvas(janela, width=largura_tela, height=altura_tela)
+canvas.pack()
+
+letras = 'abcdefghijklmnopqrstuvwxyz'
+botoes_letras = []
+for i, letra in enumerate(letras):
+    btn_letra = tk.Button(janela, text=letra, width=4, height=2, font=("Helvetica", 18),
+                          command=lambda l=letra: verificar_letra(l))
+    botoes_letras.append(btn_letra)
+    col = i % 13
+    row = i // 13
+    btn_letra.place(relx=0.1 + col * 0.07, rely=0.75 + row * 0.1)  
+
+btn_novo_jogo = tk.Button(janela, text="Jogar Novamente", width=15, height=2, font=("Helvetica", 18),
+                          command=iniciar_novo_jogo)
+
+label_palavra = tk.Label(janela, text="", font=("Courier", 36, "bold"))
+label_palavra.place(relx=0.5, rely=0.3, anchor="center")
+
+label_dica = tk.Label(janela, text="", font=("Helvetica", 24))
+label_dica.place(relx=0.5, rely=0.4, anchor="center")
+
+aviso_letra_usada = tk.Label(janela, text="", font=("Helvetica", 18), fg="red")
+aviso_letra_usada.place(relx=0.5, rely=0.65, anchor="center")  
+resultado_jogo = tk.Label(janela, text="", font=("Helvetica", 36, "bold"))
+resultado_jogo.place(relx=0.5, rely=0.8, anchor="center")
+
+T = 0
+Animais = [
+    'gato', 'cachorro', 'elefante', 'girafa', 'leão', 'macaco', 'panda',
+    'tigre', 'urso', 'zebra', 'cobra', 'rato', 'galinha', 'porco'
+]
+Frutas = [
+    'maçã', 'banana', 'abacaxi', 'laranja', 'cereja', 'melão', 'abacate',
+    'morango', 'kiwi', 'jabuticaba', 'amendoim', 'amêndoa', 'amora',
+    'ameixa', 'pitaya'
+]
+Biomas = [
+    'savana', 'deserto', 'montanha', 'floresta', 'praia', 'pantanal',
+    'mar', 'oceano', 'pradaria', 'planície', 'planalto', 'caverna',
+    'tundra', 'taiga', 'temperado', 'tropical'
+]
+TUDO = [Animais, Frutas, Biomas]
+
+iniciar_novo_jogo()
+
+janela.mainloop()
